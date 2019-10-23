d2309d9a9e52f490a16c13d82e97704301b2330c
Mikkel <mekt@itu.dk>
2019-10-23 10:41:16 +0200

    StartSession activity added

---
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/StartSession.java
@@ -0,0 +1,14 @@
+package com.mikkelthygesen.android.tid_bma_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_session);
+    }
+}
