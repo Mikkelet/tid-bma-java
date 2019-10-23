5562a02ca0b24d7a55c6a06592e7e37fde101048
Mikkel <mekt@itu.dk>
2019-10-23 11:00:49 +0200

    Added Timer fragment

---
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
@@ -0,0 +1,31 @@
+package com.mikkelthygesen.android.tid_bma_java;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Timer extends Fragment {
+
+
+    public Timer() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timer, container, false);
+    }
+
+}
