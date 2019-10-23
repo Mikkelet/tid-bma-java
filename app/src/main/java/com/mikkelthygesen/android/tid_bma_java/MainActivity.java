a153330b99027a9263e97f29492d49c8a9ffb0b1
Mikkel <mekt@itu.dk>
2019-10-23 11:16:59 +0200

    added bottom nav bar

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/MainActivity.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/MainActivity.java
@@ -3,11 +3,18 @@ package com.mikkelthygesen.android.tid_bma_java;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNavigationMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationMenu = findViewById(R.id.bottomNavigationView);
     }
 }
