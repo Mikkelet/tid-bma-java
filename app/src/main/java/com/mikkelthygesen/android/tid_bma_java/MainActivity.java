27a88041cfe4861f8a634c0bf9ab747f9aa6c541
Mikkel <mekt@itu.dk>
2019-10-23 10:34:07 +0200

    androidX

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/MainActivity.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/MainActivity.java
@@ -1,6 +1,6 @@
 package com.mikkelthygesen.android.tid_bma_java;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
