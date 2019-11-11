af296a454ca15cdaef8aaed26952c8046d89ca50
Jkoj <jkoj@itu.dk>
2019-11-11 13:31:58 +0100

    Database

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
@@ -7,12 +7,16 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.xw.repo.BubbleSeekBar;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Timer extends Fragment {
 
+
+    private BubbleSeekBar bubbleSeekBar;
 
     public Timer() {
         // Required empty public constructor
@@ -24,15 +28,24 @@ public class Timer extends Fragment {
         
         Timer fragment = new Timer();
         fragment.setArguments(args);
+
         return fragment;
     }
+
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        bubbleSeekBar = v.findViewById(R.id.bubbleSeekBarExercise);
+        
+        Database.getinstance().bubbleSeekBarExercise = bubbleSeekBar.getProgress();
+        return v;
     }
 
 }
