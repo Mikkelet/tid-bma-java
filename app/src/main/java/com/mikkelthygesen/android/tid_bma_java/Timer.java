d69e8d889af4d315c59f8539a305d9f547a8656b
Jkoj <jkoj@itu.dk>
2019-11-29 13:02:26 +0100

    button clicked xml

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
@@ -1,18 +1,16 @@
 package com.mikkelthygesen.android.tid_bma_java;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
 import com.xw.repo.BubbleSeekBar;
 
 
@@ -48,7 +46,7 @@ public class Timer extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_timer, container, false);
-        mStartSessionButtonActivateBlock = v.findViewById(R.id.StartSessionButtonActivateBlock);
+       /* mStartSessionButtonActivateBlock = v.findViewById(R.id.StartSessionButtonActivateBlock);
         mStartSessionButtonActivateBlock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,7 +60,8 @@ public class Timer extends Fragment {
                 intent.setAction("com.android.chrome");
                 startActivity(intent);
             }
-        });
+        });*/
+
 
         bubbleSeekBarExercise = v.findViewById(R.id.bubbleSeekBarExercise);
 
