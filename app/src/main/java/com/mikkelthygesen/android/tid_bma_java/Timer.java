c4904e8402caa2d7343517f8e561d680073e6460
Jkoj <jkoj@itu.dk>
2019-11-29 13:10:39 +0100

    redirect to duolingo works

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
@@ -1,13 +1,16 @@
 package com.mikkelthygesen.android.tid_bma_java;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -25,6 +28,7 @@ public class Timer extends Fragment {
     private int exerciseProgress;
     private int blockedAppsProgress;
     private Button mStartSessionButtonActivateBlock;
+    private int buttonState = 1;
 
     public Timer() {
         // Required empty public constructor
@@ -46,8 +50,9 @@ public class Timer extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_timer, container, false);
-       /* mStartSessionButtonActivateBlock = v.findViewById(R.id.StartSessionButtonActivateBlock);
-        mStartSessionButtonActivateBlock.setOnClickListener(new View.OnClickListener() {
+       mStartSessionButtonActivateBlock = v.findViewById(R.id.StartSessionButtonActivateBlock);
+
+       mStartSessionButtonActivateBlock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PackageManager packageManager = getContext().getPackageManager();
@@ -60,7 +65,9 @@ public class Timer extends Fragment {
                 intent.setAction("com.android.chrome");
                 startActivity(intent);
             }
-        });*/
+        });
+
+       
 
 
         bubbleSeekBarExercise = v.findViewById(R.id.bubbleSeekBarExercise);
