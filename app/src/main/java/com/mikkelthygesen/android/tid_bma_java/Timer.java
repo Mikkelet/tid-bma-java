816c02ad601e7b806a66cf86eb93330bdcc5e402
Jkoj <jkoj@itu.dk>
2019-12-01 12:29:31 +0100

    Functionality of activate button now green when activated

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/Timer.java
@@ -1,16 +1,13 @@
 package com.mikkelthygesen.android.tid_bma_java;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -28,7 +25,10 @@ public class Timer extends Fragment {
     private int exerciseProgress;
     private int blockedAppsProgress;
     private Button mStartSessionButtonActivateBlock;
-    private int buttonState = 1;
+    private Button activate;
+    private Button btnDemo;
+    private boolean isDefault = true;
+
 
     public Timer() {
         // Required empty public constructor
@@ -50,7 +50,7 @@ public class Timer extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_timer, container, false);
-       mStartSessionButtonActivateBlock = v.findViewById(R.id.StartSessionButtonActivateBlock);
+     /*  mStartSessionButtonActivateBlock = v.findViewById(R.id.StartSessionButtonActivateBlock);
 
        mStartSessionButtonActivateBlock.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,8 +66,20 @@ public class Timer extends Fragment {
                 startActivity(intent);
             }
         });
+        */
 
-       
+
+        activate = v.findViewById(R.id.StartSessionButtonActivateBlock);
+
+        activate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isDefault = !isDefault;
+                int resId = isDefault ? R.drawable.activate_block_circle_default : R.drawable.activate_block_circle_pressed;
+                activate.setBackgroundResource(resId);
+                activate.setText(isDefault ? "Start block" : "Activated");
+            }
+        });
 
 
         bubbleSeekBarExercise = v.findViewById(R.id.bubbleSeekBarExercise);
