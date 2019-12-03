5003ad353ea7207fe7e61bc5e3844bab667052a5
MHMagnus <m_mamsen@hotmail.com>
2019-12-03 21:14:44 +0100

    mangler at sætte de blokerede op i toppen.

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/BlockedAppDB.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/BlockedAppDB.java
@@ -28,6 +28,8 @@ public class BlockedAppDB {
         edit.apply();
     }
 
+
+
     public static List<BlockedItem> collectAllApplicationsOnPhone(final PackageManager packageManager, FragmentActivity activity) {
         ArrayList<BlockedItem> blockedItems = new ArrayList<>();
 
