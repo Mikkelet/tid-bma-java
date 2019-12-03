98de539969dce3db7099634823445615f8c5ee8b
Gabriel Frejberg Agger-Nielsen <gaag@itu.dk>
2019-12-03 12:41:00 +0100

    Update MainActivity.java

--- app/src/main/java/com/mikkelthygesen/android/tid_bma_java/MainActivity.java
+++ app/src/main/java/com/mikkelthygesen/android/tid_bma_java/MainActivity.java
@@ -7,27 +7,27 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private StartSession mStartSessionFragment = StartSession.newInstance();
-
-    private BottomNavigationView mBottomNavigationMenu;
+    private StartSession mStartSessionFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        openFragment(mStartSessionFragment);
+        if(mStartSessionFragment == null){
+            mStartSessionFragment = StartSession.newInstance();
+            openFragment(mStartSessionFragment);
+        }
         getSelectedExerciseProvider();
+
     }
 
     private void openFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.MainFrameLayout, fragment);
-        transaction.addToBackStack(null);
         transaction.commit();
     }
 
